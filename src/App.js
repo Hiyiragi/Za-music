8dc0afd000b82dacee7127768a8886873072c417
Hiyiragi <128255052+Hiyiragi@users.noreply.github.com>
2023-05-17 00:03:51 +0200

    UI setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "styles/Theme";
+import {
+  ButtonText,
+  MainTitle,
+  SectionSubtitle,
+  SectionTitle,
+  SubText,
+  Text,
+} from "components/UI/Typography";
+import { GlobalStyles } from "styles/Global";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Heart,
+  Music,
+  Pause,
+  Play,
+  Search,
+  SkipLeft,
+  SkipRight,
+  Users,
+  Volume,
+} from "components/UI/Icons";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <MainTitle>Hello</MainTitle>
+      <SectionTitle>Hello</SectionTitle>
+      <SectionSubtitle>Hello</SectionSubtitle>
+      <Text>Hello</Text>
+      <SubText>Hello</SubText>
+      <ButtonText>Hello</ButtonText>
+      <br />
+      <Play />
+      <SkipLeft />
+      <SkipRight />
+      <Pause />
+      <Volume />
+      <Music />
+      <ArrowLeft />
+      <ArrowRight />
+      <Heart />
+      <Users />
+      <Search />
+    </ThemeProvider>
+  );
 }
 
 export default App;
